1a755daeb7b976defd58f32abefc2b444c77fc31
Nazar Sterniuk <nsternuk@gmail.com>
2025-09-16 14:54:49 +0300

    Лабораторна робота №1

---
+++ lab1/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"lab1","c":"Lab1SterniukKI306","l":"Lab1SterniukKI306()","u":"%3Cinit%3E()"},{"p":"lab1","c":"Lab1SterniukKI306","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
